530a1ef98330fe0baf31f50a38bbbef6cd1eedbd
Anthony Bordg <apdb3@cam.ac.uk>
2020-11-18 15:16:15 +0000

    Add files via upload

---
+++ Field.thy
@@ -0,0 +1,30 @@
+theory Field (* Can't use the plural Fields because HOL.Fields *)
+  imports "Jacobson_Basic_Algebra.Ring_Theory"
+
+begin
+
+section \<open>Fields\<close>
+
+locale field = ring R "(+)" "(\<cdot>)" "\<zero>" "\<one>" 
+  for R and addition (infixl "+" 65) and multiplication (infixl "\<cdot>" 70) and zero ("\<zero>") and unit ("\<one>") +
+  assumes commutative_mult: "\<lbrakk> a \<in> R; b \<in> R \<rbrakk> \<Longrightarrow> a \<cdot> b = b \<cdot> a" and nonzero: "\<zero> \<noteq> \<one>" and 
+invertible_mult: "a \<in> R \<Longrightarrow> a \<noteq> \<zero> \<Longrightarrow> \<exists>b\<in>R. a \<cdot> b = \<one>"
+begin
+
+lemma left_inv_mult_is_right_inv:
+  assumes "a \<in> R" and "b \<in> R" and "a \<cdot> b = \<one>"
+  shows "b \<cdot> a = \<one>"
+  using assms commutative_mult by simp
+
+lemma uniqueness_of_inv: 
+  assumes "a \<in> R" and "a \<noteq> \<zero>" and "b \<in> R" and "c \<in> R" and "a \<cdot> b = \<one>" and "a \<cdot> c = \<one>"
+  shows "b = c" sorry
+
+end
+
+locale subfield = subring S R "(+)" "(\<cdot>)" "\<zero>" "\<one>" 
+  for S and R and addition (infix "+" 65) and multiplication (infix "\<cdot>" 70) and zero ("\<zero>") and unit ("\<one>") +
+  assumes stable_under_inv: "\<lbrakk>a \<in> S; b \<in> R\<rbrakk> \<Longrightarrow> a \<noteq> \<zero> \<Longrightarrow> a \<cdot> b = \<one> \<Longrightarrow> b \<in> S" 
+
+
+end
