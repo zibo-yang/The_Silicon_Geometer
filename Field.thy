76c3bf3a8c33732e4e68b2c081ab19b216e26651
Anthony Bordg <apdb3@cam.ac.uk>
2020-11-19 16:57:59 +0000

    Updates Field.thy

--- Field.thy
+++ Field.thy
@@ -34,19 +34,45 @@ locale subfield = subring S R "(+)" "(\<cdot>)" "\<zero>" "\<one>"
 
 section \<open>Algebraically Closed Fields\<close>
 
-locale alg_closed_field = field F "(+)" "(\<cdot>)" "\<zero>" "\<one>"
-  for F::"'a::comm_semiring_0 set" and addition (infix "+" 65) and multiplication (infix "\<cdot>" 70) and zero ("\<zero>") and unit ("\<one>") +
-  assumes alg_closed: "\<forall>(p::'a poly). Polynomial.degree p > 0 \<longrightarrow> (\<exists>r. poly p r = 0)" 
+locale alg_closed_field = field k "(+)" "(\<cdot>)" "\<zero>" "\<one>"
+  for k::"'a::comm_semiring_0 set" and addition (infix "+" 65) and multiplication (infix "\<cdot>" 70) and zero ("\<zero>") and unit ("\<one>") +
+  assumes alg_closed: "\<forall>(f::'a poly). Polynomial.degree f > 0 \<longrightarrow> (\<exists>p. poly f p = 0)" 
 (* 
 The assumption alg_closed above should probably be expressed using univariate polynomials seen as 
 a special case of multivariate polynomials as defined in the archive "Executable Multivariate Polynomials" 
 *)
 
-(*
 begin
 
-Possibly some equivalent formulations of the property of being algebraically closed
+(* Possibly some equivalent formulations of the property of being algebraically closed *)
+(* If n=1 in the blueprint then the affine space is just carrier k. *)
+
+(* def. 0.0.3 *)
+definition zero_set:: "'a poly set \<Rightarrow> 'a set"
+  where "zero_set T \<equiv> {p \<in> k. \<forall>f\<in>T. poly f p = 0}"
+
+(* def 0.0.4 *)
+definition is_alg:: "'a set \<Rightarrow> bool"
+  where "is_alg Y \<equiv> Y \<subseteq> k \<and> (\<exists>(T::'a poly set). Y = zero_set T)"
+
+(* exercise 0.0.5 *)
+lemma empty_set_is_alg:
+  shows "is_alg empty" sorry
+
+lemma whole_space_is_alg:
+  shows "is_alg k" sorry
+
+(* exercise 0.0.6 *)
+lemma inter_of_alg_family_is_alg:
+  fixes f::"'a set set"
+  assumes "Y \<in> f \<Longrightarrow> is_alg Y"
+  shows "is_alg (\<Inter>f)" sorry
+
+(* exercise 0.0.7 *)
+lemma union_of_two_alg_is_alg:
+  assumes "is_alg Y" and "is_alg Z"
+  shows "is_alg (Y \<union> Z)" sorry
  
 end
-*)
+
 end
