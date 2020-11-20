5a5809e7f9c85067fcb5e585ab8614461755d3dd
Anthony Bordg <apdb3@cam.ac.uk>
2020-11-20 10:10:17 +0000

    Updates Field.thy

--- Field.thy
+++ Field.thy
@@ -2,6 +2,7 @@ theory Field (* Can't use the plural Fields because HOL.Fields *)
   imports "Jacobson_Basic_Algebra.Ring_Theory"
           (* "Polynomials.MPoly_PM" *)
           "HOL-Computational_Algebra.Polynomial"
+          "HOL.Topological_Spaces"
 
 begin
 
@@ -52,26 +53,50 @@ definition zero_set:: "'a poly set \<Rightarrow> 'a set"
   where "zero_set T \<equiv> {p \<in> k. \<forall>f\<in>T. poly f p = 0}"
 
 (* def 0.0.4 *)
-definition is_alg:: "'a set \<Rightarrow> bool"
-  where "is_alg Y \<equiv> Y \<subseteq> k \<and> (\<exists>(T::'a poly set). Y = zero_set T)"
+definition algebraic:: "'a set \<Rightarrow> bool"
+  where "algebraic Y \<equiv> Y \<subseteq> k \<and> (\<exists>(T::'a poly set). Y = zero_set T)"
 
 (* exercise 0.0.5 *)
-lemma empty_set_is_alg:
-  shows "is_alg empty" sorry
+lemma empty_set_is_algebraic:
+  shows "algebraic empty" sorry
 
-lemma whole_space_is_alg:
-  shows "is_alg k" sorry
+lemma whole_space_is_algebraic:
+  shows "algebraic k" sorry
 
 (* exercise 0.0.6 *)
-lemma inter_of_alg_family_is_alg:
+lemma inter_of_alg_family_is_algebraic:
   fixes f::"'a set set"
-  assumes "Y \<in> f \<Longrightarrow> is_alg Y"
-  shows "is_alg (\<Inter>f)" sorry
+  assumes "Y \<in> f \<Longrightarrow> algebraic Y"
+  shows "algebraic (\<Inter>f)" sorry
 
 (* exercise 0.0.7 *)
-lemma union_of_two_alg_is_alg:
-  assumes "is_alg Y" and "is_alg Z"
-  shows "is_alg (Y \<union> Z)" sorry
+lemma union_of_two_algebraic_is_algebraic:
+  assumes "algebraic Y" and "algebraic Z"
+  shows "algebraic (Y \<union> Z)" sorry
+
+(* def. 0.0.8 *)
+definition zariski_open:: "'a set \<Rightarrow> bool"
+  where "zariski_open S \<equiv> \<exists>Y. algebraic Y \<and> S = k - Y"
+
+(* exercise 0.0.9 *)
+lemma zariski_topology_is_topology:
+  shows "topology zariski_open" sorry
+
+definition  zariski_closed:: "'a set \<Rightarrow> bool"
+  where "zariski_closed S \<equiv> zariski_open (k - S)"
+
+(* def. 0.0.10 *)
+definition irreducible:: "'a set \<Rightarrow> bool"
+  where "irreducible S \<equiv> S \<subseteq> k \<and> S \<noteq> empty \<and> \<not>(\<exists>U V. S = U \<union> V \<and> zariski_closed U \<and> 
+zariski_closed V \<and> U \<subset> k \<and> V \<subset> k)"
+
+(* def 0.0.11 *)
+definition aff_alg_variety:: "'a set \<Rightarrow> bool"
+  where "aff_alg_variety S \<equiv> irreducible S \<and> zariski_closed S"
+
+(* def 0.0.12 *)
+definition quasi_aff_variety:: "'a set \<Rightarrow> bool"
+  where "quasi_aff_variety U \<equiv> \<exists>S Y. aff_alg_variety S \<and> zariski_open Y \<and> U = S \<inter> Y"
  
 end
 
