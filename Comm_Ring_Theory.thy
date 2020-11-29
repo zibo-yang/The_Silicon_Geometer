ddd3670b90e86a37e52307a13e4a5a98eadbb693
Anthony Bordg <apdb3@cam.ac.uk>
2020-11-29 14:42:02 +0000

    Add files via upload

---
+++ Comm_Ring_Theory.thy
@@ -0,0 +1,13 @@
+theory Comm_Ring_Theory
+  imports "Jacobson_Basic_Algebra.Ring_Theory"
+
+begin
+
+section \<open>Commutative Rings\<close>
+
+locale comm_ring = ring R "(+)" "(\<cdot>)" "\<zero>" "\<one>" 
+  for R and addition (infixl "+" 65) and multiplication (infixl "\<cdot>" 70) and zero ("\<zero>") and unit ("\<one>") +
+  assumes commutative_mult: "\<lbrakk> a \<in> R; b \<in> R \<rbrakk> \<Longrightarrow> a \<cdot> b = b \<cdot> a"
+
+
+end
