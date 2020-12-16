057083d603cba132bbd214e3808fb6a625fe10b6
Anthony Bordg <apdb3@cam.ac.uk>
2020-12-16 18:31:37 +0000

    syntactic sugar for preimages added

--- Set_Further_Theory.thy
+++ Set_Further_Theory.thy
@@ -6,4 +6,7 @@ begin
 definition complement_in_of:: "'a set \<Rightarrow> 'a set \<Rightarrow> 'a set" ("_\<setminus>_")
   where "A \<setminus> B \<equiv> {x. x \<in> A \<and> x \<notin> B}"
 
+definition preimage:: "('a \<Rightarrow> 'b) \<Rightarrow> 'b set \<Rightarrow> 'a set" ("_ \<^sup>\<inverse> _")
+  where "f\<^sup>\<inverse> V \<equiv> {x. f x \<in> V}"
+
 end
