5a9d72ae7ee14f2b78fe9192049adf89b028b143
Anthony Bordg <apdb3@cam.ac.uk>
2020-12-17 16:53:29 +0000

    inverse map with syntactic sugar added

--- Set_Further_Theory.thy
+++ Set_Further_Theory.thy
@@ -3,10 +3,22 @@ theory Set_Further_Theory
 
 begin
 
+section \<open>Sets\<close>
+
 definition complement_in_of:: "'a set \<Rightarrow> 'a set \<Rightarrow> 'a set" ("_\<setminus>_")
   where "A \<setminus> B \<equiv> {x. x \<in> A \<and> x \<notin> B}"
 
+section \<open>Functions\<close>
+
 definition preimage:: "('a \<Rightarrow> 'b) \<Rightarrow> 'b set \<Rightarrow> 'a set" ("_ \<^sup>\<inverse> _")
   where "f\<^sup>\<inverse> V \<equiv> {x. f x \<in> V}"
+
+definition inverse_map:: "('a \<Rightarrow> 'b) \<Rightarrow> 'a set \<Rightarrow> 'b set \<Rightarrow> ('b \<Rightarrow> 'a)" ("_\<^sup>\<inverse> _ _")
+  where "f\<^sup>\<inverse> S T \<equiv> restrict (inv_into S f) T"
+
+lemma
+  assumes "bijective_map f S T"
+  shows "bijective_map (f\<^sup>\<inverse> S T) T S"
+  sorry
 
 end
