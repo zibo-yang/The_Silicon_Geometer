13d9c825d1e4ad533f4ddd8ad93af733521bd473
paulson <lp15@cam.ac.uk>
2020-12-28 12:46:25 +0000

    set theory operators now abbreviations referring to the originals

--- Set_Further_Theory.thy
+++ Set_Further_Theory.thy
@@ -5,20 +5,25 @@ begin
 
 section \<open>Sets\<close>
 
-definition complement_in_of:: "'a set \<Rightarrow> 'a set \<Rightarrow> 'a set" ("_\<setminus>_")
-  where "A \<setminus> B \<equiv> {x. x \<in> A \<and> x \<notin> B}"
+abbreviation complement_in_of:: "'a set \<Rightarrow> 'a set \<Rightarrow> 'a set" ("_\<setminus>_" [65,65]65)
+  where "A \<setminus> B \<equiv> A-B"
 
 section \<open>Functions\<close>
 
-definition preimage:: "('a \<Rightarrow> 'b) \<Rightarrow> 'b set \<Rightarrow> 'a set" ("_ \<^sup>\<inverse> _")
-  where "f\<^sup>\<inverse> V \<equiv> {x. f x \<in> V}"
+abbreviation preimage:: "('a \<Rightarrow> 'b) \<Rightarrow> 'b set \<Rightarrow> 'a set" (infixr " \<^sup>\<inverse>" 90)
+  where "f\<^sup>\<inverse> V \<equiv> vimage f V"
 
-definition inverse_map:: "('a \<Rightarrow> 'b) \<Rightarrow> 'a set \<Rightarrow> 'b set \<Rightarrow> ('b \<Rightarrow> 'a)" ("_\<^sup>\<inverse> _ _")
+definition inverse_map:: "('a \<Rightarrow> 'b) \<Rightarrow> 'a set \<Rightarrow> 'b set \<Rightarrow> ('b \<Rightarrow> 'a)" ("_\<^sup>\<inverse>")
   where "f\<^sup>\<inverse> S T \<equiv> restrict (inv_into S f) T"
 
-lemma
+lemma bijective_map_preimage:
   assumes "bijective_map f S T"
   shows "bijective_map (f\<^sup>\<inverse> S T) T S"
-  sorry
+proof
+  show "f\<^sup>\<inverse> S T \<in> T \<rightarrow>\<^sub>E S"
+    by (simp add: assms bij_betw_imp_funcset bij_betw_inv_into bijective.bijective bijective_map.axioms(2) inverse_map_def)
+  show "bij_betw (f\<^sup>\<inverse> S T) T S"
+    using assms by (simp add: bij_betw_inv_into bijective_def bijective_map_def inverse_map_def)
+qed
 
 end
