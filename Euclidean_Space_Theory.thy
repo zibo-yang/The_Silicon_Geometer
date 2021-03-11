be79e22fbcc892c2d8c497a4f9a6da30942cd3ee
Anthony Bordg <apdb3@cam.ac.uk>
2021-03-11 17:26:07 +0000

    Update

--- Euclidean_Space_Theory.thy
+++ Euclidean_Space_Theory.thy
@@ -1,6 +1,6 @@
 theory Euclidean_Space_Theory
   imports Group_Further_Theory
-          HOL.Real
+          Complex_Main
 
 begin
 
@@ -9,15 +9,15 @@ section \<open>Real Vector Spaces\<close>
 
 locale real_vector_space = abelian_group V add zero 
   for V and add (infixl "+" 70) and zero ("\<zero>") + 
-  fixes scale:: "real \<Rightarrow> 'a \<Rightarrow> 'a" (infixr "*\<^sub>R" 75)
-  assumes scale_add: "r *\<^sub>R (x + y) = r *\<^sub>R x + r *\<^sub>R y"
-  and add_scale: "(r + s) *\<^sub>R x = r *\<^sub>R x + s *\<^sub>R x"
-  and scale_scale: "r *\<^sub>R s *\<^sub>R x = (r * s) *\<^sub>R x"
-  and one_scale: "1 *\<^sub>R x = x"
+  fixes scale:: "real \<Rightarrow> 'a \<Rightarrow> 'a" (infixr "\<cdot>\<^sub>R" 75)
+  assumes scale_add: "r \<cdot>\<^sub>R (x + y) = r \<cdot>\<^sub>R x + r \<cdot>\<^sub>R y"
+  and add_scale: "(r + s) \<cdot>\<^sub>R x = r \<cdot>\<^sub>R x + s \<cdot>\<^sub>R x"
+  and scale_scale: "r \<cdot>\<^sub>R s \<cdot>\<^sub>R x = (r * s) \<cdot>\<^sub>R x"
+  and one_scale: "1 \<cdot>\<^sub>R x = x"
 begin
 
 abbreviation divide:: "'a \<Rightarrow> real \<Rightarrow> 'a"  (infixl "'/\<^sub>R" 70)
-  where "x /\<^sub>R r \<equiv> Fields.inverse r *\<^sub>R x"
+  where "x /\<^sub>R r \<equiv> Fields.inverse r \<cdot>\<^sub>R x"
 
 end (* real_vector_space *)
 
@@ -26,7 +26,7 @@ section \<open>Inner Product Spaces\<close>
 
 locale inner_product_space = real_vector_space +
   fixes inner_prod:: "'a \<Rightarrow> 'a \<Rightarrow> real" ("\<langle>_,_\<rangle>")
-  assumes scale_is_linear: "\<langle>r *\<^sub>R x, y\<rangle> = r * \<langle>x, y\<rangle>"
+  assumes scale_is_linear: "\<langle>r \<cdot>\<^sub>R x, y\<rangle> = r * \<langle>x, y\<rangle>"
 and add_is_linear: "\<langle>x + y, z\<rangle> = \<langle>x,z\<rangle> + \<langle>y,z\<rangle>"
 and is_symmetric: "\<langle>x,y\<rangle> = \<langle>y,x\<rangle>"
 and is_positive_definite: "x \<noteq> \<zero> \<Longrightarrow> \<langle>x,x\<rangle> > 0"
@@ -52,12 +52,12 @@ locale euclidean_vector_space = inner_product_space +
 and dim [simp]: "card Basis = n"
 and inner_basis: "\<lbrakk>u \<in> Basis; v \<in> Basis\<rbrakk> \<Longrightarrow> \<langle>u,v\<rangle> = (if u = v then 1 else 0)"
 and euclidean_all_zero_iff: "(\<forall>u\<in>Basis. \<langle>x,u\<rangle> = 0) \<longleftrightarrow> (x = \<zero>)"
-(* the two last axioms should be deduced from two axioms asserting that the family Basis is free
-and spans V *)
+(* the two last axioms should be deduced from two axioms asserting that Basis is free and spans V *)
 begin
 
 definition dist:: "'a \<Rightarrow> 'a \<Rightarrow> real" ("\<d>'(_,_')")
-  where "\<d>(u,v) \<equiv> sqrt (\<langle>u \<hyphen> v , u \<hyphen> v\<rangle>)"
+  where "\<d>(u,v) \<equiv> sqrt (\<langle>minus u v , minus u v\<rangle>)"
+(* Why the notation for minus from Group_Further_Theory.thy does not work here ? *)
 
 end (* euclidean_vector_space *)
 
