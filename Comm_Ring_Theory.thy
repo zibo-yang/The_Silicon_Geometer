03e16e4ff4e84c6d9f07d9641456c163072d485f
Anthony Bordg <apdb3@cam.ac.uk>
2021-03-16 15:56:23 +0000

    sketch `induced_morphism_with_id_is_local`

--- Comm_Ring_Theory.thy
+++ Comm_Ring_Theory.thy
@@ -4884,7 +4884,7 @@ locale ind_mor_btw_stalks = morphism_ringed_spaces +
 begin
 
 interpretation stx:stalk X is_open\<^sub>X \<O>\<^sub>X \<rho>\<^sub>X b add_str\<^sub>X mult_str\<^sub>X zero_str\<^sub>X one_str\<^sub>X 
-  "{U. is_open\<^sub>X U \<and> x \<in> U}" 
+  "{U. is_open\<^sub>X U \<and> x \<in> U}" "x"
 proof qed (auto simp: is_elem)
 
 interpretation stfx: stalk Y is_open\<^sub>Y \<O>\<^sub>Y \<rho>\<^sub>Y d add_str\<^sub>Y mult_str\<^sub>Y zero_str\<^sub>Y one_str\<^sub>Y 
@@ -5284,7 +5284,25 @@ lemma (in locally_ringed_space) induced_morphism_with_id_is_local:
   shows "ind_mor_btw_stalks.is_local 
 S is_open \<FF> \<rho> add_str mult_str zero_str one_str is_open \<FF> \<rho> add_str mult_str zero_str one_str
 (identity S) x V (\<phi>\<^bsub>S is_open \<FF> \<rho> is_open \<FF> \<rho> (identity S) (\<lambda>U. identity (\<FF> U)) x\<^esub>)"
-  sorry (* using induced_morphism_with_id_is_id id_is_local_ring_morphism *)
+ (* sorry using induced_morphism_with_id_is_id id_is_local_ring_morphism *)
+proof-
+  interpret stfx: stalk S is_open \<FF> \<rho> b add_str mult_str zero_str one_str "{U. is_open U \<and> (identity S x) \<in> U}" "identity S x"
+    sorry
+  have "local_ring stfx.carrier_stalk stfx.add_stalk stfx.mult_stalk (stfx.zero_stalk V) (stfx.one_stalk V)"
+    using assms stalks_are_local sorry
+  interpret stx: stalk S is_open \<FF> \<rho> b add_str mult_str zero_str one_str "{U. is_open U \<and> x \<in> U}" "x"
+    sorry
+  have "local_ring stx.carrier_stalk stx.add_stalk stx.mult_stalk (stx.zero_stalk ((identity S)\<^sup>\<inverse> S V)) (stx.one_stalk ((identity S)\<^sup>\<inverse> S V))" 
+  proof- 
+    have "((identity S)\<^sup>\<inverse> S V) = V" using assms(2) sorry
+    thus ?thesis sorry
+  qed
+  moreover have "\<phi>\<^bsub>S is_open \<FF> \<rho> is_open \<FF> \<rho> (identity S) (\<lambda>U. identity (\<FF> U)) x\<^esub> = identity stx.carrier_stalk"
+    using induced_morphism_with_id_is_id stx.is_elem by simp
+  ultimately show ?thesis 
+    using assms id_is_local_ring_morphism Comm_Ring_Theory.ind_mor_btw_stalks.is_local_def 
+    sorry
+qed
 
 (* definition 0.45 *)
 
